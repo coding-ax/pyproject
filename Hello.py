7128f393e52215456d4ef0efe1dbabc5a1793f30
whutfaker <1312976124@qq.com>
2020-03-19 23:11:56 +0800

    first push

---
+++ Hello.py
@@ -0,0 +1,29 @@
+name = "adt ddd"
+print(name.title())
+# python使用#号作为注释
+
+# magicans = ['libai', 'liubei', 'baijuyi', 'daqiao', 'wangzherongayo']
+
+# for magican in magicans:
+#     print(magican.title())
+#     print("thank you Mr " + magican.title())
+#     for magican in magicans:
+#         print("come here" + magican)
+#     print("ok! Mrs AX")
+# print("Thank you all,nice to meet you again")
+
+# for item in range(1, 5):
+#     print(item)
+# print(range(1, 5)) # range(1, 5)
+# print(list(range(1, 5)))  # [1,2,3,4]
+
+# # 列表解析
+# jiexis = [value ** 2 for value in range(1, 6)]
+# print(jiexis)
+
+# # 切片：
+# print(magicans[5:3])
+
+#元组
+dimentions = (50, 200)
+
