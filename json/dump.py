0854a6bac4241f5ef5cefd604c87bd6f8ab14955
whutfaker <1312976124@qq.com>
2020-03-22 14:22:55 +0800

    停电了“

--- json/dump.py
+++ json/dump.py
@@ -1,7 +1,9 @@
 import json
 numbers = [1, 3, 5, 7, 9, 11, 13]
 
+#json.dump() 用于存储数据
 filename = 'dump.json'
 with open(filename, 'w', encoding='utf8') as file_object:
     #第一个参数是要存储的数据，第二个参数是可以用于存储数据的文件对象
     json.dump(numbers, file_object)
+
