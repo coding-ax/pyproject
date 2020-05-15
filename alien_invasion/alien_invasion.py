f552fd027c559209343484b836686e41aefc23cc
whutfaker <1312976124@qq.com>
2020-05-15 23:09:28 +0800

    alien

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,7 +24,6 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
         # 每次循环都重回屏幕
         screen.fill(ai_settings.bg_color)
         # 让最近绘制的屏幕可见
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,4 +5,4 @@ class Settings():
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color=(230,230,230)
+        self.bg_color = (230, 230, 230)
